eb0926fb6d9911c2f69d4d2c5c29e2c255a96fb8
LuanSantos00 <luancosmefilho@gmail.com>
2022-07-12 09:43:16 -0300

    Array e tipos genericos

---
+++ ArrayETiposGenericos/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista<int> idades = new Lista<int>();
+
+            idades.Adicionar(5);
+            idades.AdicionarVarios(1, 5, 78);
+
+            int idadeSoma = 0;
+            for (int i = 0; i < idades.Tamanho; i++)
+            {
+                int idadeAtual = idades[i];
+
+                idadeSoma += idadeAtual;
+
+            }
+
+            Console.WriteLine(SomarVarios(1, 2, 3, 4, 5));
+
+           
+            Console.ReadLine();
+        }
+
+        static void TestaListaDeObject()
+        {
+            ListaDeObject listaDeIdades = new ListaDeObject();
+
+            listaDeIdades.Adicionar(10);
+            listaDeIdades.Adicionar(5);
+            listaDeIdades.Adicionar(4);
+            listaDeIdades.AdicionarVarios(16, 23, 60);
+
+
+            for (int i = 0; i < listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+                Console.WriteLine($"Idade no indice {i}: {idade}");
+            }
+
+        }
+        static void TestaListaDeContaCorrente()
+        {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contaDoGui = new ContaCorrente(1111, 111111);
+
+            ContaCorrente[] contas = new ContaCorrente[]
+           {
+                contaDoGui,
+                new ContaCorrente(874, 567987),
+                new ContaCorrente(874, 445668)
+
+           };
+
+            lista.AdicionarVarios(contas);
+
+            for (int i = 0; i < lista.Tamanho; i++)
+            {
+                ContaCorrente itemAtual = lista.GetItemNoIndice(i);
+                Console.WriteLine($"Item na posição {i} = Conta {itemAtual.Numero} / {itemAtual.Agencia}");
+            }
+
+        }
+        static int SomarVarios(params int[] numeros)
+        {
+            int acumulador = 0;
+            foreach (int numero in numeros)
+            {
+                acumulador += numero;
+            }
+
+            return acumulador;
+        }
+
+        static void TestaArrayDeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+           {
+                new ContaCorrente(874, 567987),
+                new ContaCorrente(874, 445668),
+                new ContaCorrente(874, 7781438)
+           };
+
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaATual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaATual.Numero}");
+            }
+        }
+        static void TestaArrayInt()
+        {
+            // ARRAY de inteiros, com 5 posições.
+            int[] idades = new int[6];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+            idades[5] = 60;
+
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+
+                acumulador += idade;
+
+                int media = acumulador / idades.Length;
+
+                Console.WriteLine($"Média de idades = {media}");
+            }
+
+
+
+        }
+    }
+}
